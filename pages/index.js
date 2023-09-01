a8686b050b6acd418fcad1bf83435f91c34cacde
Zstrike11 <143776696+Zstrike11@users.noreply.github.com>
2023-09-01 00:23:56 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my app" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by clicking <code>pages/index.js</code>
         </p>
       </main>
 
